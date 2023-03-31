d66348f81af072f81790cc7e4b381356db038e21
lidi <lidi@patsnap.com>
2023-03-31 14:00:40 +0800

    remove comment lines

--- extension.js
+++ extension.js
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const _ = require("lodash");
 const vscode = require("vscode");
 const { Configuration, OpenAIApi } = require("openai");
@@ -11,16 +9,8 @@ let proxyPort = '7890';
 let proxyProtocol = 'http';
 let configDisposable;
 
-// vscode.window.showInformationMessage('Hello World!');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
 
-  // load token file
   configDisposable = vscode.workspace.onDidChangeConfiguration(({ affectsConfiguration }) => {
     let hasChange = affectsConfiguration('refactoring-tools');
     if (hasChange) {
